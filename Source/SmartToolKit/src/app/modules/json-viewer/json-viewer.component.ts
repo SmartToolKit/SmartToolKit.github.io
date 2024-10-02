c60edc9f2db9a8ba5502ef9272827b485b1b1851
saman azadi <samanazadi1996@gmail.com>
2024-10-03 03:09:45 +0330

    Publish in 03-Thu-10_ 3-09

--- Source/SmartToolKit/src/app/modules/json-viewer/json-viewer.component.ts
+++ Source/SmartToolKit/src/app/modules/json-viewer/json-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -8,18 +8,34 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './json-viewer.component.html',
   styleUrls: ['./json-viewer.component.scss']
 })
-export class JsonViewerComponent {
+export class JsonViewerComponent implements AfterViewInit {
+
+  page = "Format";
+  ngAfterViewInit(): void {
+    this.page = localStorage.getItem("json-viewer-page") ?? "Format";
+    this.jsonContent = localStorage.getItem("json-viewer-jsonContent") ?? "{}";
+
+    if (this.page == "Format" || this.page == "Remove white space") {
+      this.jsonFormatter(this.page)
+    } else {
+      this.jsonViewer();
+    }
+  }
+
+  save() {
+    localStorage.setItem("json-viewer-page", this.page);
+    localStorage.setItem("json-viewer-jsonContent", this.jsonContent);
+  }
 
   jsonContent: string = "";
 
-  jsonFormatter(type: number) {
+  jsonFormatter(type: string) {
     try {
-debugger
-      if (type == 1) {
-        this.jsonContent = JSON.stringify(JSON.parse(this.jsonContent));
-
-      } else {
+      this.page = type;
+      if (type == "Format") {
         this.jsonContent = JSON.stringify(JSON.parse(this.jsonContent), null, "\t");
+      } else {
+        this.jsonContent = JSON.stringify(JSON.parse(this.jsonContent));
       }
       console.log('Formatted JSON:', this.jsonContent);
     } catch (error) {
@@ -29,10 +45,13 @@ debugger
 
   jsonViewer() {
     try {
+      this.page = "Viewer";
+
       alert('todo!');
 
     } catch (error) {
       alert('Invalid JSON input!');
     }
   }
+  download() { }
 }
