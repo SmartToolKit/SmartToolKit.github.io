df6cc3005d3fec6689f21d91fb241dfe6ebf392e
saman azadi <samanazadi1996@gmail.com>
2024-10-03 20:06:00 +0330

    Publish in 03-Thu-10_20-05

--- _Build.cmd
+++ _Build.cmd
@@ -5,14 +5,15 @@ REM Navigate to the source directory
 cd ".\Source\SmartToolKit\"
 
 REM Execute ng build and check for errors
-call ng build
+call npm install
+call ng build --output-path ./../../docs
 if %errorlevel% neq 0 (
     echo Build failed!
     exit /b %errorlevel%
 )
 
 REM Copy files using xcopy with the /y switch to overwrite existing files
-xcopy ".\dist\smart-tool-kit\browser\" "..\.." /s /e /h /i /y
+xcopy ".\..\..\docs\browser\" ".\..\..\docs\" /s /e /h /i /y
 if %errorlevel% neq 0 (
     echo Copy failed!
     exit /b %errorlevel%
