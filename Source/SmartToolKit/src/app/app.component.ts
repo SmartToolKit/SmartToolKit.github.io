8df38b562aca4fa9c0241bd8e86d6400be1f1632
saman azadi <samanazadi1996@gmail.com>
2024-10-03 20:37:03 +0330

    Publish in 03-Thu-10_20-36

--- Source/SmartToolKit/src/app/app.component.ts
+++ Source/SmartToolKit/src/app/app.component.ts
@@ -10,19 +10,21 @@ export class AppComponent {
   title = 'SmartToolKit';
   constructor(private swUpdate: SwUpdate) {
     if (this.swUpdate.isEnabled) {
-      alert("A new version of the application is available. Please refresh to get the latest updates.")
-      // swal.fire({
-      //   position: 'center',
-      //   icon: 'success',
-      //   title: 'Update Available!',
-      //   text: 'A new version of the application is available. Please refresh to get the latest updates.',
-      //   showConfirmButton: true,
-      //   confirmButtonText: 'Refresh Now',
-      // }).then(function () {
-      //   window.location.reload();
-      // })
+      this.swUpdate.activateUpdate().then(() => {
+        alert("A new version of the application is available. Please refresh to get the latest updates.")
+        // swal.fire({
+        //   position: 'center',
+        //   icon: 'success',
+        //   title: 'Update Available!',
+        //   text: 'A new version of the application is available. Please refresh to get the latest updates.',
+        //   showConfirmButton: true,
+        //   confirmButtonText: 'Refresh Now',
+        // }).then(function () {
+        //   window.location.reload();
+        // })
 
+      });
     }
+    
   }
-
 }
