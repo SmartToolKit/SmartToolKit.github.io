85961103b3e13c1a7d5a66205700016fb1c78422
saman azadi <samanazadi1996@gmail.com>
2024-10-05 03:18:47 +0330

    update image-resizer.component.html and image-resizer.component.ts

--- Source/SmartToolKit/src/app/modules/image-resizer/image-resizer.component.ts
+++ Source/SmartToolKit/src/app/modules/image-resizer/image-resizer.component.ts
@@ -3,45 +3,81 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-image-resizer',
   templateUrl: './image-resizer.component.html',
-  styleUrl: './image-resizer.component.scss'
+  styleUrls: ['./image-resizer.component.scss']
 })
 export class ImageResizerComponent {
 
-  result: any[] = []
-  openFile(event: Event): void {
+  result: any[] = [];
 
+  openFile(event: Event): void {
     const fileInput = event.target as HTMLInputElement;
 
     if (fileInput.files && fileInput.files.length > 0) {
-
       for (let index = 0; index < fileInput.files.length; index++) {
-        var file = fileInput.files[index]
-        var t = window.URL || window.webkitURL;
-        var objectUrl = t.createObjectURL(file);
+        const file = fileInput.files[index];
+        const t = window.URL || window.webkitURL;
+        const objectUrl = t.createObjectURL(file);
 
-        debugger
-        var model = {
-          id: 'id' + (new Date()).getTime() + "-" + index,
-          file: file,
-          name:file.name,
-          image: objectUrl,
-          size:Math.round(file.size/1000) +" kb"
-        }
-        this.result.push(model)
+        const img = new Image();
+        img.src = objectUrl;
+
+        img.onload = () => {
+          const model = {
+            id: 'id' + (new Date()).getTime() + "-" + index,
+            file: file,
+            name: file.name,
+            image: objectUrl,
+            size: Math.round(file.size / 1000) + " kb",
+            width: img.naturalWidth,  // Get image width
+            height: img.naturalHeight, // Get image height
+            orgWidth: img.naturalWidth,  // Get image width
+            orgHeight: img.naturalHeight, // Get image height
+            newName: '',
+            type: file.name.split(".").pop(),
+            aspectRatio: img.naturalWidth / img.naturalHeight // Calculate aspect ratio
+          };
+
+          model.newName = model.name.substring(0, file.name.lastIndexOf('.')) + `-${model.width}X${model.height}`;
+          this.result.push(model);
+        };
       }
-
     }
   }
-  delete(id: number) {
-    this.result = this.result.filter(p => p.id != id)
-  }
-  duplicate(id: number) {
-    var model = this.result.filter(p => p.id == id)[0]
 
-    this.result.push({
-      id: 'id' + (new Date()).getTime() + "d",
-      file: model.file,
-      image: model.image
-    })
+  delete(id: number): void {
+    this.result = this.result.filter(p => p.id != id);
+  }
+
+  duplicate(id: number): void {
+    const model = this.result.find(p => p.id == id);
+    if (model) {
+      this.result.push({
+        id: 'id' + (new Date()).getTime() + "d",
+        file: model.file,
+        name: model.name,
+        image: model.image,
+        size: (model.size /1000) +" kb",
+        orgWidth: model.orgWidth,
+        orgHeight: model.orgHeight,
+        width: model.width,
+        height: model.height,
+        newName: model.newName,
+        type: model.type,
+        aspectRatio: model.aspectRatio
+      });
+    }
+  }
+
+  updateSize(item: any, type: string): void {
+    if (type === 'w') {
+      // Update height based on new width while maintaining aspect ratio
+      item.height = Math.round(item.width / item.aspectRatio);
+    } else if (type === 'h') {
+      // Update width based on new height while maintaining aspect ratio
+      item.width = Math.round(item.height * item.aspectRatio);
+    }
+
+    // Update the new name with the modified dimensions
+    item.newName = item.name.substring(0, item.name.lastIndexOf('.')) + `-${item.width}X${item.height}`;
   }
 }
