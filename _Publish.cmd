ad7aca6ccd74fb2df909d84c4d837ee240ddb1b9
saman azadi <samanazadi1996@gmail.com>
2024-10-04 04:55:29 +0330

    Publish in 2024-10-04- 4-55

--- _Publish.cmd
+++ _Publish.cmd
@@ -1,14 +1,20 @@
 @echo off
 setlocal
 
-:: Get current date and time
-for /f "tokens=1-4 delims=/ " %%a in ("%date%") do set "date=%%c-%%a-%%b"
-for /f "tokens=1-2 delims=:." %%a in ("%time%") do set "time=%%a-%%b"
-set "datetime=%date%_%time%"
+:: Get current date in YYYY-MM-DD format
+for /f "tokens=2-4 delims=/.- " %%a in ('echo %date%') do (
+    if "%%c" neq "" (
+        set "date=%%c-%%a-%%b"
+    ) else (
+        for /f "tokens=1-3 delims=.-/ " %%x in ("%date%") do set "date=%%x-%%y-%%z"
+    )
+)
 
-:: Format datetime for filename and remove any invalid characters
-set "datetime=%datetime::=-%"
-set "datetime=%datetime:.=-%"
+:: Get current time in HHMM format (24-hour clock)
+for /f "tokens=1-2 delims=:." %%a in ("%time%") do set "time=%%a-%%b"
+
+:: Combine date and time
+set "datetime=%date%-%time%"
 
 :: Ask the user if they want to publish
 set /p userInput=Do you want to publish changes? (yes/no): 
