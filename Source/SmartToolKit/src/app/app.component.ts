0502e52159989b5b6e8cd1351627ac367c79c83a
saman azadi <samanazadi1996@gmail.com>
2024-10-11 17:07:46 +0330

    Publish in 2024-10-11-17-07

--- Source/SmartToolKit/src/app/app.component.ts
+++ Source/SmartToolKit/src/app/app.component.ts
@@ -11,17 +11,23 @@ export class AppComponent {
   title = 'SmartToolKit';
   constructor(private swUpdate: SwUpdate) {
     if (this.swUpdate.isEnabled) {
-      this.swUpdate.activateUpdate().then((rrr) => {
-        swal.fire({
-          position: 'center',
-          icon: 'success',
-          title: 'Update Available!',
-          text: 'A new version of the application is available. Please refresh to get the latest updates.',
-          showConfirmButton: true,
-          confirmButtonText: 'Refresh Now',
-        }).then(function () {
-          window.location.reload();
-        })
+      this.swUpdate.checkForUpdate().then(isUpdateAvailable => {
+        if (isUpdateAvailable) {
+          this.swUpdate.activateUpdate().then(() => {
+            swal.fire({
+              position: 'center',
+              icon: 'success',
+              title: 'Update Available!',
+              text: 'A new version of the application is available. Please refresh to get the latest updates.',
+              showConfirmButton: true,
+              confirmButtonText: 'Refresh Now',
+            }).then(() => {
+              window.location.reload();
+            });
+          });
+        }
+      }).catch(err => {
+        console.error('Error checking for update', err);
       });
     }
 
