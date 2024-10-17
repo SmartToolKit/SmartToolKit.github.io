dc37b6e3ce698bbaafc9cadcf90a9fd551ef8787
Saman Azadi <60857846+samanazadi1996@users.noreply.github.com>
2024-10-17 15:12:25 +0330

    Update dashboard.component.ts

--- Source/SmartToolKit/src/app/modules/dashboard/dashboard.component.ts
+++ Source/SmartToolKit/src/app/modules/dashboard/dashboard.component.ts
@@ -11,25 +11,12 @@ export class DashboardComponent {
   constructor(public router: Router, private titleService: Title) {
     this.titleService.setTitle("Smart ToolKit")
   }
-
-  tools = [
-    // {
-    //   title: "Code Snapshot",
-    //   description: "Turn your code snippets into visually stunning images, perfect for sharing or documentation.",
-    //   icon:"./assets/images/code-snapshot.svg",
-    //   url: "code-snapshot"
-    // },
+tools = [
     {
       title: "JSON Viewer",
       description: "Easily view and format JSON data, making it more readable and easier to work with.",
       icon: "./assets/images/json-viewer.svg",
       url: "json-viewer"
-    },
-    {
-      title: "GUID Generator",
-      description: "Generate unique GUIDs for use in your applications and databases.",
-      icon: "./assets/images/guid-generator.svg",
-      url: "guid-generator"
     },
     {
       title: "JWT Viewer",
@@ -38,48 +25,55 @@ export class DashboardComponent {
       url: "jwt-viewer"
     },
     {
+      title: "GUID Generator",
+      description: "Generate unique GUIDs for use in your applications and databases.",
+      icon: "./assets/images/guid-generator.svg",
+      url: "guid-generator"
+    },
+    {
+      title: "Rsa Key Generator",
+      description: "Generate RSA keys for cryptographic purposes.",
+      icon: "./assets/images/rsa-key-generator.svg",
+      url: "rsa-key-generator"
+    },
+    {
+      title: "Regex Tester",
+      description: "Test and validate regular expressions.",
+      icon: "./assets/images/regex-tester.svg",
+      url: "regex-tester"
+    },
+    {
       title: "Convert To Base64",
-      description: "",
+      description: "Convert your files or text into Base64 format.",
       icon: "./assets/images/convert-to-base64.svg",
       url: "convert-to-base64"
     },
     {
       title: "Image Resizer",
-      description: "",
+      description: "Resize images to desired dimensions.",
       icon: "./assets/images/image-resizer.svg",
       url: "image-resizer"
     },
     {
-      title: "Regex Tester",
-      description: "",
-      icon: "./assets/images/regex-tester.svg",
-      url: "regex-tester"
-    },
-    {
-      title: "Rsa Key Generator",
-      description: "",
-      icon: "./assets/images/rsa-key-generator.svg",
-      url: "rsa-key-generator"
-    },
-    {
       title: "JavaScript Minifier",
-      description: "",
+      description: "Minify your JavaScript code for better performance.",
       icon: "./assets/images/java-script-minifier.svg",
       url: "java-script-minifier"
     },
     {
       title: "Css Minifier",
-      description: "",
+      description: "Minify your CSS files to reduce file size.",
       icon: "./assets/images/css-minifier.svg",
       url: "css-minifier"
     },
     {
       title: "Svg Editor",
-      description: "",
+      description: "Edit and optimize your SVG files.",
       icon: "./assets/images/svg-editor.svg",
       url: "svg-editor"
     }
-  ];
+];
+
   goto(item: any) {
     this.router.navigate([item.url])
   }
