89781ac31ca8cdf4e71ac31c193d782d030df6b8
saman azadi <samanazadi1996@gmail.com>
2024-10-25 14:36:48 +0330

    Publish in 2024-10-25-14-36

--- Source/SmartToolKit/src/app/modules/dashboard/dashboard.component.ts
+++ Source/SmartToolKit/src/app/modules/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+import { json } from 'stream/consumers';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,89 +11,123 @@ import { Router } from '@angular/router';
 export class DashboardComponent {
   constructor(public router: Router, private titleService: Title) {
     this.titleService.setTitle("Smart ToolKit")
+    this.loadBookmarks()
+    this.sort();
   }
-tools = [
+  tools = [
     {
       title: "JSON Viewer",
       description: "Easily view and format JSON data, making it more readable and easier to work with.",
       icon: "./assets/images/json-viewer.svg",
-      url: "json-viewer"
+      url: "json-viewer",
+      bookmark: false
     },
     {
       title: "JWT Viewer",
       description: "Decode and verify JWT tokens with this easy-to-use tool.",
       icon: "./assets/images/jwt-viewer.svg",
-      url: "jwt-viewer"
+      url: "jwt-viewer",
+      bookmark: false
     },
     {
       title: "GUID Generator",
       description: "Generate unique GUIDs for use in your applications and databases.",
       icon: "./assets/images/guid-generator.svg",
-      url: "guid-generator"
+      url: "guid-generator",
+      bookmark: false
     },
     {
       title: "Rsa Key Generator",
       description: "Generate RSA keys for cryptographic purposes.",
       icon: "./assets/images/rsa-key-generator.svg",
-      url: "rsa-key-generator"
+      url: "rsa-key-generator",
+      bookmark: false
     },
     {
       title: "Regex Tester",
       description: "Test and validate regular expressions.",
       icon: "./assets/images/regex-tester.svg",
-      url: "regex-tester"
+      url: "regex-tester",
+      bookmark: false
     },
     {
       title: "Convert To Base64",
       description: "Convert your files or text into Base64 format.",
       icon: "./assets/images/convert-to-base64.svg",
-      url: "convert-to-base64"
+      url: "convert-to-base64",
+      bookmark: false
     },
     {
       title: "Image Resizer",
       description: "Resize images to desired dimensions.",
       icon: "./assets/images/image-resizer.svg",
-      url: "image-resizer"
+      url: "image-resizer",
+      bookmark: false
     },
     {
       title: "JavaScript Minifier",
       description: "Minify your JavaScript code for better performance.",
       icon: "./assets/images/java-script-minifier.svg",
-      url: "java-script-minifier"
+      url: "java-script-minifier",
+      bookmark: false
     },
     {
       title: "Css Minifier",
       description: "Minify your CSS files to reduce file size.",
       icon: "./assets/images/css-minifier.svg",
-      url: "css-minifier"
+      url: "css-minifier",
+      bookmark: false
     },
     {
       title: "Svg Editor",
       description: "Edit and optimize your SVG files.",
       icon: "./assets/images/svg-editor.svg",
-      url: "svg-editor"
+      url: "svg-editor",
+      bookmark: false
     },
     {
       title: "QR Code Generator",
-      description: "",
+      description: "Generate QR codes quickly and easily for URLs, text, or other data.",
       icon: "./assets/images/qrcode-generator.svg",
-      url: "qrcode-generator"
+      url: "qrcode-generator",
+      bookmark: false
     },
     {
       title: "BarCode Generator",
-      description: "",
+      description: "Create barcodes for various formats, ideal for product labeling and inventory management.",
       icon: "./assets/images/barcode-generator.svg",
-      url: "barcode-generator"
+      url: "barcode-generator",
+      bookmark: false
     },
     {
       title: "Code Snapshot",
-      description: "",
+      description: "Capture and save code snippets with syntax highlighting for sharing or documentation.",
       icon: "./assets/images/code-snapshot.svg",
-      url: "code-snapshot"
+      url: "code-snapshot",
+      bookmark: false
     }
-];
-
+  ];
+  onBookmarkClick(event: MouseEvent, item: any): void {
+    event.preventDefault();
+    event.stopPropagation();
+    item.bookmark = !item.bookmark
+    this.sort();
+    localStorage.setItem("dashboard-bookmark", JSON.stringify(this.tools.filter(p => p.bookmark).map(p => p.url)))
+  }
+  sort() {
+    this.tools = this.tools.sort((a, b) => Number(b.bookmark) - Number(a.bookmark));
+  }
   goto(item: any) {
     this.router.navigate([item.url])
   }
+  loadBookmarks() {
+    const bookmarkSaved = localStorage.getItem("dashboard-bookmark");
+    if (!bookmarkSaved) return;
+
+    const savedBookmarks = JSON.parse(bookmarkSaved) as string[];
+    this.tools.forEach(tool => {
+      tool.bookmark = savedBookmarks.includes(tool.url);
+    });
+  }
+
 }
